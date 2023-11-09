20edc4455a80df93dc7887ecc2838058203f9989
William Moore <w.moore@dundee.ac.uk>
2023-11-09 11:21:00 +0000

    Create new images from labels in _seg datasets

--- scripts/rois.py
+++ scripts/rois.py
@@ -2,7 +2,7 @@ import logging
 import os
 import omero
 from omero.cli import cli_login
-from omero.gateway import BlitzGateway
+from omero.gateway import BlitzGateway, DatasetWrapper
 from omero_rois import mask_from_binary_image
 from omero.rtypes import rstring
 import zarr
@@ -14,9 +14,49 @@ RGBA = (255, 255, 0, 128)
 DRYRUN = False
 
 
+def create_image(conn, cells_data, nuclei_data, name, dataset):
+
+    # find or create new Dataset
+    target_name = f"{dataset.name}_seg"   # same as idr0079
+    project = dataset.getParent()
+    new_dataset = None
+    for ds in project.listChildren():
+        if ds.name == target_name:
+            new_dataset = ds
+    if new_dataset is None:
+        ds = omero.model.DatasetI()
+        ds.name = rstring(target_name)
+        ds = conn.getUpdateService().saveAndReturnObject(ds, conn.SERVICE_OPTS)
+        link = omero.model.ProjectDatasetLinkI()
+        link.parent = omero.model.ProjectI(project.id, False)
+        link.child = omero.model.DatasetI(ds.id.val, False)
+        conn.getUpdateService().saveAndReturnObject(link)
+        new_dataset = DatasetWrapper(conn, ds)
+
+    # ensure all data is same dtype. nuclei was int8, cells is int32
+    nuclei_data = nuclei_data.astype(cells_data.dtype)
+
+    sizeZ = cells_data.shape[0]
+    sizeC = 2
+    
+    def plane_gen():
+        for z in range(sizeZ):
+            for c in range(sizeC):
+                yield(cells_data[z] if c == 0 else nuclei_data[z])
+
+    image = conn.createImageFromNumpySeq(plane_gen(), name, sizeZ=sizeZ, sizeC=sizeC, dataset=new_dataset)
+    logging.info(f"Created new image: {image.id}")
+    conn.setChannelNames("Image", [image.id], {1: "Cells", 2: "Nuclei"})
+    # turn on just the Cells, with glasbey LUT to highlight segmentation
+    image.setActiveChannels([1], colors=['glasbey.lut'])
+    image.saveDefaults()
+
+
 def get_images(conn):
     project = conn.getObject('Project', attributes={'name': PROJECT})
     for dataset in project.listChildren():
+        if "_seg" in dataset.name:
+            continue
         for image in dataset.listChildren():
             if "labels" in image.name:
                 continue
@@ -70,11 +110,11 @@ def create_rois(img_data, size_x, size_y, is_nuclei):
         for i in range(size_z):
             plane = img_data[i]
             print(f"Nuclei plane {i}")
-            mask = scale_mask_from_binary_image(plane == 2, size_x, size_y, RGBA, i, None, None, "Nucleis", False)
+            mask = scale_mask_from_binary_image(plane == 2, size_x, size_y, RGBA, i, None, None, "Nuclei", False)
             if mask:
                 logging.info(f"Found nuclei masks for plane {i}")
                 roi = omero.model.RoiI()
-                roi.setName(rstring("Nucleis"))
+                roi.setName(rstring("Nuclei"))
                 roi.addShape(mask)
                 rois.append(roi)
             else:
@@ -119,7 +159,7 @@ def get_labels_data(image, name):
 def main():
     logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s [%(pathname)s, %(lineno)s]')
     with cli_login() as c:
-        conn = omero.gateway.BlitzGateway(client_obj=c.get_client())
+        conn = BlitzGateway(client_obj=c.get_client())
         for ds, im in get_images(conn):
             size_y = im.getSizeY()
             size_x = im.getSizeX()
@@ -136,6 +176,10 @@ def main():
                     rois.extend(create_rois(nuc_data, size_x, size_y, True))
                 if not DRYRUN and len(rois) > 0:
                     save_rois(conn, im, rois)
+
+                # Create new Image
+                if nuc_data is not None or cells_data is not None:
+                    create_image(conn, cells_data, nuc_data, im.name, ds)
             except Exception as e:
                 logging.warning(e)
 
