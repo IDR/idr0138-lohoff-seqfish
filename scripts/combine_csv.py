d24e5697cff053695c11f5ad7c866424d198e56c
Dominik Lindner <d.lindner@dundee.ac.uk>
2024-08-08 13:27:28 +0100

    Add scripts to updated processed files

---
+++ scripts/combine_csv.py
@@ -0,0 +1,83 @@
+import csv
+import os
+import re
+
+PROCESSED_FILE_DIR = "../processed_files"
+
+DS_0 = "TimEmbryos-102219"
+DS_1 = "TimEmbryos-120919"
+
+# Area,Centroid_1,Centroid_2,BoundingBox_1,BoundingBox_2,BoundingBox_3,BoundingBox_4,cellID,z
+# 732848,1162.89787786826,248.662740431849,0.5,0.5,2048,1168,1,2
+#HEADER_CELL = "# header roi,image,s,l,d,d,d,d,l,l,l,l"
+FIELDS_CELL = "Image,Roi,Shape,ImageName,Area,Centroid_1,Centroid_2,BoundingBox_1,BoundingBox_2,BoundingBox_3,BoundingBox_4,cellID,z".split(',')
+
+# cellID,geneID,regionID,x,y,z,seeds,intensity
+# 135,Abcc4,1,670.522,423.266,2,4,671
+#HEADER_SEG = "# header roi,image,s,l,s,l,d,d,l,l,l"
+FIELDS_SEG = "Image,Roi,Shape,ImageName,cellID,geneID,regionID,x,y,z,seeds,intensity".split(',')
+
+def create_writer(ds, is_cell):
+    if is_cell:
+        filename = f"{PROCESSED_FILE_DIR}/{ds}-cellData-processed.csv"
+    else:
+        filename = f"{PROCESSED_FILE_DIR}/{ds}-segmentedData-processed.csv"
+    outfile = open(filename, mode="w")
+    if is_cell:
+        writer = csv.DictWriter(outfile, fieldnames=FIELDS_CELL)
+    else:
+        writer = csv.DictWriter(outfile, fieldnames=FIELDS_SEG)
+    writer.writeheader()
+    return outfile, writer
+
+outfiles = []
+writers = dict()
+for d in DS_0, DS_1:
+    for c in True, False:
+        o, w = create_writer(d, c)
+        outfiles.append(o)
+        writers[f"{d}{c}"] = w
+
+for file in os.scandir(PROCESSED_FILE_DIR):
+    # TimEmbryos-102219-Pos0-cellData.csv
+    # TimEmbryos-102219-Pos0-segmentedData.csv
+    filename = file.name
+
+    if "Pos" not in filename:
+        continue
+
+    print(f"Processing {filename} ...")
+
+    is_cell = "cellData" in filename
+    is_seg = "segmentedData" in filename
+    if not is_seg and not is_cell:
+        print(f"Skipping {filename}")
+        continue
+
+    pat = re.compile(r"TimEmbryos-(?P<ds>.+)-(?P<pos>.+)-.+")
+    m = pat.match(filename).groupdict()
+    m['ds'] = f"TimEmbryos-{m['ds']}"
+    img_name = f"MMStack_{m['pos']}.ome.zarr"
+
+    key = f"{m['ds']}{is_cell}"
+    if not key in writers:
+        print(f"Can't find {key}")
+        continue
+
+    with open(file, mode="r") as csvfile:
+        reader = csv.DictReader(csvfile)
+        fnames = reader.fieldnames.copy()
+        fnames.insert(0, "ImageName")
+        fnames.insert(0, "Shape")
+        fnames.insert(0, "Roi")
+        fnames.insert(0, "Image")
+
+        for row in reader:
+            row["Image"] = "-1"
+            row["Roi"] = "-1"
+            row["Shape"] = "-1"
+            row["ImageName"] = img_name
+            writers[key].writerow(row)
+
+for outfile in outfiles:
+    outfile.close()
---
+++ scripts/update_csv.py
@@ -0,0 +1,95 @@
+import csv
+import os
+
+import omero.cli
+from omero.gateway import BlitzGateway
+
+DS_0 = "TimEmbryos-102219"
+DS_1 = "TimEmbryos-120919"
+
+PROCESSED_FILE_DIR = "../processed_files"
+OUT_DIR = "../processed_files_finished"
+
+PROJECT_NAME = "idr0138-lohoff-seqfish/experimentA"
+
+HEADER_CELL = "# header image,roi,l,s,l,d,d,d,d,l,l,l,l"
+HEADER_SEG = "# header image,roi,l,s,l,s,l,d,d,l,l,l"
+
+def get_reader(ds, is_cell):
+    if is_cell:
+        filename = f"{PROCESSED_FILE_DIR}/{ds}-cellData-processed.csv"
+    else:
+        filename = f"{PROCESSED_FILE_DIR}/{ds}-segmentedData-processed.csv"
+    infile = open(filename, mode="r")
+    reader = csv.DictReader(infile)
+    return infile, reader
+
+
+def get_roi_id(conn, image, z, roi_name, roi_cache):
+    if not roi_cache:
+        roi_service = conn.getRoiService()
+        roi_cache = roi_service.findByImage(image.getId(), None).rois
+    for roi in roi_cache:
+        if roi.getName().getValue() == roi_name:
+            shape = roi.copyShapes()[0]
+            if shape.theZ.getValue() == z:
+                return roi.getId().getValue(), shape.getId().getValue(), roi_cache
+    print(f"WARN: Could not find {roi_name} on z={z} for {image.getName()}")
+    return None, None, roi_cache
+
+
+with omero.cli.cli_login() as c:
+    conn = BlitzGateway(client_obj=c.get_client())
+    project = conn.getObject("Project", attributes={"name": PROJECT_NAME})
+    for cell in True, False:
+        for dataset in project.listChildren():
+            if DS_0 in dataset.getName():
+                ifile, reader = get_reader(DS_0, cell)
+            elif DS_1 in dataset.getName():
+                ifile, reader = get_reader(DS_1, cell)
+            else:
+                print(f"WARN: Can't handle {dataset.getName()}")
+                continue
+
+            images = dict()
+            roi_cache = dict()
+            for img in dataset.listChildren():
+                images[img.getName()] = img
+                roi_cache[img.getName()] = None
+
+            if cell:
+                ofilename = f"{OUT_DIR}/{dataset.getName()}_cellData.csv"
+            else:
+                ofilename = f"{OUT_DIR}/{dataset.getName()}_segmentedData.csv"
+
+            if os.path.exists(ofilename):
+                print(f"Skipping existing file {ofilename}")
+                continue
+
+            with open(ofilename, mode="w") as ofile:
+                print(f"Processing {ofilename} ...")
+                if cell:
+                    ofile.write(f"{HEADER_CELL}\n")
+                else:
+                    ofile.write(f"{HEADER_SEG}\n")
+                writer = csv.DictWriter(ofile, fieldnames=reader.fieldnames.copy())
+                writer.writeheader()
+                for row in reader:
+                    img_name = row['ImageName']
+                    if img_name not in images:
+                        print(f"WARN: Can't find {img_name}")
+                        writer.writerow(row)
+                        continue
+                    row['Image'] = images[img_name].getId()
+
+                    roi_name = f"Cell {row['cellID']}"
+                    z_plane = int(row['z'])
+                    roi_id, shape_id, roi_cache[img_name] = get_roi_id(conn, images[img_name], z_plane, roi_name, roi_cache[img_name])
+                    if not roi_id:
+                        print(f"Can't find {roi_name} for {img_name}")
+                        writer.writerow(row)
+                        continue
+                    row['Roi'] = roi_id
+                    row['Shape'] = shape_id
+                    writer.writerow(row)
+            ifile.close()
