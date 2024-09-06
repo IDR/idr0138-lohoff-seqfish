e9fa28705cba703c6a636cc54a16b82ed4a9912f
Dominik Lindner <d.lindner@dundee.ac.uk>
2024-09-06 14:54:38 +0100

    Fix attach csv script

--- scripts/attach_csv.sh
+++ scripts/attach_csv.sh
@@ -4,9 +4,12 @@
 #
 # Run script like this: ./attach_csv.sh <CSV file>
 
+# This will need https://github.com/ome/omero-metadata/pull/86
+
 set -euo pipefail
 
 filename=$1
 datasetname=${filename%_*.csv}
+tablename=${filename##*_}
 datasetid=`omero hql -q --style plain --ids-only "select d.id from Dataset d where d.name = '${datasetname}'" | cut -d, -f2`
-omero metadata populate Dataset:$datasetid --file $filename
+omero metadata populate Dataset:$datasetid --allow_nan --table-name $tablename --file $filename
